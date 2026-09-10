04f4d1a6f1e95466c775c8dd0184045848e5cd28
Morgan Schulz <mschulz0451@aol.com>
2026-09-10 14:09:49 -0500

    Add Equipment Editor entrypoint

---
+++ src/main/java/com/example/equipmenteditor/EquipmentEditorMod.java
@@ -0,0 +1,12 @@
+package com.example.equipmenteditor;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(EquipmentEditorMod.MOD_ID)
+public final class EquipmentEditorMod {
+    public static final String MOD_ID = "equipmenteditor";
+
+    public EquipmentEditorMod(IEventBus modBus) {
+    }
+}
